f180fff1befff75bb6930eebea7ebbe5d35882ee
arul1321 <syahrulrahmadan819@gmail.com>
2023-01-13 16:56:35 +0700

    testing

---
+++ options/config.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//━━━━━━━━━━━━━━━[ WEBSITE APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIs = {
+ZeroYT7: 'https://zeroyt7-api.xyz',
+}
+
+//━━━━━━━━━━━━━━━[ APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIKeys = {
+'https://zeroyt7-api.xyz': 'Your Apikey',
+}
+
+//━━━━━━━━━━━━━━━[ SETTING ]━━━━━━━━━━━━━━━━━//
+
+global.owner = ['6281578859076']
+global.premium = ['6281578859076']
+global.botname = 'Z-Bot'
+global.packname = 'Z-Bot MD'
+global.author = 'Z-Bot'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+success: '✓ Success',
+admin: 'Fitur Khusus Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+owner: 'Fitur Khusus Owner Bot',
+group: 'Fitur Digunakan Hanya Untuk Group!',
+private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Khusus Pengguna Nomor Bot',
+wait: 'Loading...',
+endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+premium: "Infinity",
+free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
